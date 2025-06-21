526250e51d32fec78ff58ed3679f7da17aa3cee9
dcbaile <dylancbailey@outlook.com>
2025-06-21 15:40:08 -0400

    completed balloon game and started on physics game

--- Balloon Popper/Balloon.gd
+++ Balloon Popper/Balloon.gd
@@ -1,11 +1,16 @@
 extends Area3D
 
-var clicks_to_pop : int = 3
-var size_increase : float = 0.2
-var score_to_give : int = 1
+@export var clicks_to_pop : int = 5
+@export var size_increase : float = 0.2
+@export var score_to_give : int = 1
 
 
 
 func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		print("Increase size")
+		scale += Vector3.ONE * size_increase
+		clicks_to_pop -= 1
+	
+	if clicks_to_pop == 0:
+		get_node("/root/Main").increase_score(score_to_give)
+		queue_free()
