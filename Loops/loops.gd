45f36d3b1dbae11a851b81c509849edfdd151bfd
dcbaile <dylancbailey@outlook.com>
2025-06-23 22:44:51 -0400

    finished loops project

--- Loops/loops.gd
+++ Loops/loops.gd
@@ -7,6 +7,13 @@ func _ready() -> void:
 	for i in spawn_count:
 		var star = star_scene.instantiate()
 		add_child(star)
+		star.position.x = randi_range(-280, 280)
+		star.position.y = randi_range(-150, 150)
+		
+		var star_size = randf_range(0.5, 1.0)
+		
+		star.scale.x = star_size
+		star.scale.y = star_size
 		
 		
 	
